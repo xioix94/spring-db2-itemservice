3c550cb0ad36fbb97917cfea4df5259b451b240b
norma <norma@normas-MacBook-Pro.local>
2024-05-02 16:55:29 +0900

    first commit

---
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -0,0 +1,26 @@
+package hello.itemservice.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Item {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "item_name")
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
